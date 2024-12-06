83478464335086ea346292ce5bd789021c4b52f6
Claca06 <clauscal.cc@gmail.com>
2024-12-06 15:25:32 +0100

    Aggiunti logging nella classe errori, e ultimo controllo su UtenteService

--- src/main/java/it/nesea/albergo/utente_ruolo/exception/GlobalExceptionHandler.java
+++ src/main/java/it/nesea/albergo/utente_ruolo/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package it.nesea.albergo.utente_ruolo.exception;
 
 import it.nesea.albergo.utente_ruolo.dto.response.CustomResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -13,37 +14,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<CustomResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
         List<String> errors = new ArrayList<>();
         ex.getBindingResult().getFieldErrors().forEach(e -> errors.add(e.getDefaultMessage()));
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(CustomResponse.error(errors));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<CustomResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getError(ex));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<CustomResponse> handleNotFoundException(NotFoundException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(getError(ex));
     }
 
     @ExceptionHandler(InstanceAlreadyExistsException.class)
     public ResponseEntity<CustomResponse> handleInstanceAlreadyExistsException(InstanceAlreadyExistsException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(getError(ex));
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<CustomResponse> handleBadCredentialsException(BadCredentialsException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(getError(ex));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<CustomResponse> handleGeneralException(Exception ex) {
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(getError(ex));
     }
 
--- src/main/java/it/nesea/albergo/utente_ruolo/service/UtenteServiceImpl.java
+++ src/main/java/it/nesea/albergo/utente_ruolo/service/UtenteServiceImpl.java
@@ -89,6 +89,10 @@ public class UtenteServiceImpl implements UtenteService {
         if (ricercaUtenteDto.getIdUtente() == null && ricercaUtenteDto.getNome() == null) {
             log.info("Ricerca utenti: nessun criterio di ricerca specificato. Inizio ricerca non filtrata");
             List<Utente> utenti = utenteRepository.findAll();
+            if(utenti.isEmpty()){
+                log.warn("Nessun utente trovato");
+                throw new NotFoundException("Nessun utente trovato");
+            }
             return utenteMapper.toDtoList(utenti);
         }
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
