3cf650f828da96eaf2628bf3264bacb09d68568a
ChristianSorace9226 <soracechristian0@gmail.com>
2024-12-12 10:15:26 +0100

    /fix aggiunto i messaggi in chiaro per la validazione della request crea utente, fixato il messaggio custom di errore in caso di requestbody mancante (risolto per tutto il ms)

--- src/main/java/it/nesea/albergo/utente_ruolo/dto/request/CreaUtenteDto.java
+++ src/main/java/it/nesea/albergo/utente_ruolo/dto/request/CreaUtenteDto.java
@@ -2,7 +2,6 @@ package it.nesea.albergo.utente_ruolo.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -15,16 +14,14 @@ public class CreaUtenteDto implements Serializable {
     @Serial
     private static final long serialVersionUID = 7646631177953932401L;
 
-    @NotNull
-    @NotBlank
-    @Size(min = 2, max = 50)
+    @NotBlank(message = "il nome non puo essere vuoto e non puo essere null")
+    @Size(min = 2, max = 50, message = "il nome non puo contenere meno di 2 caratteri e non puo essere lungo più di 50")
     private String nome;
 
-    @NotNull
-    @NotBlank
-    @Size(min = 2, max = 50)
+    @NotBlank(message = "il cognome non può essere vuoto e non può essere null")
+    @Size(min = 2, max = 50, message = "il cognome non può contenere meno di 2 caratteri e non può essere lungo più di 50")
     private String cognome;
 
-    @Min(1)
+    @Min(value = 1, message = "Il campo id ruolo deve essere maggiore o uguale a 1")
     private int idRuolo;
 }
