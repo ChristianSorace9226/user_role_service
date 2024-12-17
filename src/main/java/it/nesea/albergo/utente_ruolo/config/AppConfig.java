9f1465dacdbaa42fb8ffb9c59d2a6332b242e084
Claca06 <clauscal.cc@gmail.com>
2024-12-17 09:45:09 +0100

    micro fixing

--- src/main/java/it/nesea/albergo/utente_ruolo/config/AppConfig.java
+++ src/main/java/it/nesea/albergo/utente_ruolo/config/AppConfig.java
@@ -1,10 +1,13 @@
 package it.nesea.albergo.utente_ruolo.config;
 
 import it.nesea.albergo.common_lib.exception.GlobalExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
     public GlobalExceptionHandler globalExceptionHandler() {
         return new GlobalExceptionHandler();
     }
