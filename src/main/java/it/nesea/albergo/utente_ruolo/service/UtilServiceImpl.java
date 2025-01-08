26c7e78cdab21bb7f941980335e2e8a0e018d204
Claca06 <clauscal.cc@gmail.com>
2025-01-08 15:32:55 +0100

    metodo di utilità migliorato

--- src/main/java/it/nesea/albergo/utente_ruolo/service/UtilServiceImpl.java
+++ src/main/java/it/nesea/albergo/utente_ruolo/service/UtilServiceImpl.java
@@ -1,8 +1,11 @@
 package it.nesea.albergo.utente_ruolo.service;
 
+import it.nesea.albergo.utente_ruolo.model.entity.Utente;
 import it.nesea.albergo.utente_ruolo.model.repository.UtenteRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -18,6 +21,16 @@ public class UtilServiceImpl implements UtilService {
     public boolean isUtentePresente(Integer id) {
         log.debug("Controlla se l'utente con id {} esiste", id);
         short idShort = id.shortValue();
-        return utenteRepository.findById(idShort).isPresent();
+        Optional<Utente> utente = utenteRepository.findById(idShort);
+        if (utente.isPresent()) {
+            if (utente.get().getDataCancellazione() == null){
+                log.info("L'utente con id {} esiste e non è stato cancellato", id);
+                return true;
+            }
+            log.warn("L'utente con id {} è un utente cancellato", id);
+            return false;
+        }
+        log.info("Nessun utente trovato con id {}", id);
+        return false;
     }
 }
