bf24e30ac439bb7cfc5ffc19f986820fc1b3ae8d
Claca06 <clauscal.cc@gmail.com>
2025-01-09 10:43:47 +0100

    micro fixing

--- src/main/java/it/nesea/albergo/utente_ruolo/controller/UtilController.java
+++ src/main/java/it/nesea/albergo/utente_ruolo/controller/UtilController.java
@@ -17,7 +17,7 @@ public class UtilController {
         this.utilService = utilService;
     }
 
-    @GetMapping("/checkUtente")
+    @GetMapping("/check-utente")
     public ResponseEntity<CustomResponse<Boolean>> getUtenteById(@RequestParam Integer idUtente) {
         return ResponseEntity.ok(CustomResponse.success(utilService.isUtentePresente(idUtente)));
     }
